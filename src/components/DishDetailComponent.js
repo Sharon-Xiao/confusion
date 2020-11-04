de3dbf3cf4e64d75829e850ee1d5bbca76839829
Xiao <xyy180789@163.com>
2020-11-04 17:41:52 +0800

    Redux Thunk

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -3,9 +3,28 @@ import {Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem
 	Modal, ModalHeader, ModalBody, Button, Label, Col, Row} from 'reactstrap';
 import {Link} from 'react-router-dom';
 import {Control, LocalForm, Errors} from 'react-redux-form';
+import {Loading} from './LoadingComponent';
 
 const DishDetail = (props) => {
-	if(props.dish!=null)
+	if(props.isLoading){
+		return (
+			<div className="container">
+				<div className="row">
+					<Loading />
+				</div>
+			</div>
+		);
+	}
+	else if(props.errMess){
+		return (
+			<div className="container">
+				<div className="row">
+					<h4>{props.errMess}</h4>
+				</div>
+			</div>
+		);
+	}
+	else if(props.dish!=null)
 		return(
 			<div className="container">
 				<div className="row">
--- src/redux/dishes.js
+++ src/redux/dishes.js
@@ -1,7 +1,13 @@
-import { DISHES } from '../shared/dishes';
+import * as ActionTypes from './ActionTypes';
 
-export const Dishes=(state=DISHES, action)=>{
+export const Dishes=(state={isLoadong:true, erMess:null, dishes:[]}, action)=>{
 	switch(action.type){
+		case ActionTypes.ADD_DISHES:
+			return {...state, isLoading:false, errmMess:null, dishes:action.payload};
+		case ActionTypes.DISHES_LOADING:
+			return {...state, isLoading:true, errmMess:null, dishes:[]};
+		case ActionTypes.DISHES_FAILED:
+			return {...state, isLoading:false, errmMess:action.payload};
 		default:
 			return state;
 	}
