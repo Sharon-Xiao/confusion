e1cac31bbe1369fd839ee8a6025fbcac5bcc5c3c
Xiao <xyy180789@163.com>
2020-11-09 23:54:02 +0800

    React Animation Components

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -5,6 +5,7 @@ import {Link} from 'react-router-dom';
 import {Control, LocalForm, Errors} from 'react-redux-form';
 import {Loading} from './LoadingComponent';
 import {baseUrl} from '../shared/baseUrl';
+import {FadeTransform, Fade, Stagger} from 'react-animation-components';
 
 const DishDetail = (props) => {
 	if(props.isLoading){
@@ -56,32 +57,39 @@ const DishDetail = (props) => {
 
 function RenderDish({dish}){
 	return(
-	<Card>
-		<CardImg top src={baseUrl+dish.image} alt={dish.name} />
-		<CardBody>
-			<CardTitle>{dish.name}</CardTitle>
-			<CardText>{dish.description}</CardText>
-		</CardBody>					
-	</Card>
+		<FadeTransform in
+			transformProps={{
+				exitTransform:'scale(0.5) translateY(-50%)'
+			}}>
+			<Card>
+				<CardImg top src={baseUrl+dish.image} alt={dish.name} />
+				<CardBody>
+					<CardTitle>{dish.name}</CardTitle>
+					<CardText>{dish.description}</CardText>
+				</CardBody>					
+			</Card>
+		</FadeTransform>
 	);
 }
 
 function RenderComments({comments, postComment, dishId}){
 	if(comments!=null){
-		const commentSet=comments.map((comment) => {
-			return (
-				<li>
-					<p>{comment.comment}</p>
-					<p>-- {comment.author}, {new Intl.DateTimeFormat('en-US', {year:'numeric', month:'short', day:'2-digit'}).format(new Date(Date.parse(comment.date)))}</p>
-				</li>
-			);
-		});
-
 		return(
 			<div>
 				<h4>Comments</h4>
-				<ul>
-					{commentSet}
+				<ul className="list-unstyled">
+					<Stagger in>
+						{comments.map((comment) => {
+							return (
+								<Fade in>
+									<li key={comment.id}>
+										<p>{comment.comment}</p>
+										<p>-- {comment.author}, {new Intl.DateTimeFormat('en-US', {year:'numeric', month:'short', day:'2-digit'}).format(new Date(Date.parse(comment.date)))}</p>
+									</li>
+								</Fade>
+							);
+						})}
+					</Stagger>
 				</ul>
 				<CommentForm dishId={dishId} postComment={postComment} />
 			</div>
